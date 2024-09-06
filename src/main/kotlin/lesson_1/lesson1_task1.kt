32bc9afc45894128d52a9d03613a22f4bc3c4d04
Fallien <rdp6@mail.ru>
2024-09-06 19:05:03 +0300

    KS_1_1 create

---
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -0,0 +1,8 @@
+package org.example.lesson_1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val customersGreeting: String = "Спасибо за покупку, ждем Вас снова в нашем магазине!"
+    println(numberOfOrders)
+    println(customersGreeting)
+}
