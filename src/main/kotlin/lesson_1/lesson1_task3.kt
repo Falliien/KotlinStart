0ce4cea0488b0ee0d066a1e7b8c435d930b2d0af
Fallien <rdp6@mail.ru>
2024-09-06 21:49:34 +0300

    KS_1_3

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,18 @@
+    fun main(){
+
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+//    println(year)
+//    println("%02d".format (hour))
+//    println("%02d".format (minute))
+
+    hour = 10
+    minute = 55
+
+    print(hour)
+    print(":")
+    print(minute)
+
+}
