324b36f1637f9fb582641d244e46c89874246c97
Fallien <rdp6@mail.ru>
2024-09-07 23:32:49 +0300

    KS-1-3 изменил на стринг

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,15 +1,15 @@
     fun main(){
 
     val year = 1961
-    var hour = 9
-    var minute = 7
+    var hour = "09"
+    var minute = "07"
 
-//    println(year)
-//    println("%02d".format (hour))
-//    println("%02d".format (minute))
+    println(year)
+    println(hour)
+    println(minute)
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
 
     print(hour)
     print(":")
