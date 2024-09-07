d03e972f952773fac67e6c0b1d8a5edbd129687c
Fallien <rdp6@mail.ru>
2024-09-07 09:08:02 +0300

    KS_2_1

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    val totalStudents = 4f
+    val member1 = 3
+    val member2 = 4
+    val member3 = 3
+    val member4 = 5
+
+    val averageNumber = (member1 + member2 + member3 + member4) / totalStudents
+
+    println(averageNumber)
+
+
+}
+
