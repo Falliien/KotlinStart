9f4fdf5992ddc5956f8da1b8b3d675934dca650a
Fallien <rdp6@mail.ru>
2024-09-08 00:48:41 +0300

    KS-1-4 выполнение задания

---
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,24 @@
+fun main() {
+
+    val length: Long
+    length = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Double
+    partOfDay = 0.075
+    val seconds: Int
+    seconds = 6480
+    val partOfYear: Double
+    partOfYear = 2.0547945205479453E-4
+    val apogee: Int
+    apogee = 327000
+
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+}
+
+
+
