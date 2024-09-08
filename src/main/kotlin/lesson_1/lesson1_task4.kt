4aeb9fd022a2682cb8e83a89b7ebe484bd48582a
Fallien <rdp6@mail.ru>
2024-09-08 16:01:53 +0300

    KS-1-4 изменил два типа

--- src/main/kotlin/lesson_1/lesson1_task4.kt
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -1,16 +1,12 @@
 fun main() {
 
-    val length: Long
-    length = 40_868_600_000
+    val length: Long = 40_868_600_000
     val age: Byte = 27
-    val partOfDay: Double
-    partOfDay = 0.075
-    val seconds: Int
-    seconds = 6480
-    val partOfYear: Double
-    partOfYear = 2.0547945205479453E-4
-    val apogee: Int
-    apogee = 327000
+    val partOfDay: Float = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
 
     println(length)
     println(age)
