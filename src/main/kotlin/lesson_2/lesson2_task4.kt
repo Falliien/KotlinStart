69d9b3ee9b180cd1e9c8f8ef773cc917eb15ca1b
Fallien <rdp6@mail.ru>
2024-09-09 16:31:45 +0300

    KS-2-4 выполнение задания

---
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -0,0 +1,19 @@
+
+fun main(){
+
+    val buff = 20.toDouble()
+    val crystalMineral = 7
+    val ironMineral = 11
+
+    val crystalMineralBuff = crystalMineral * (1 + buff / 100)
+    val crystalMineralBuff1 = crystalMineralBuff - crystalMineral
+
+    val ironMineralBuff = ironMineral * (1 + buff / 100)
+    val ironMineralBuff1 =ironMineralBuff - ironMineral
+
+
+    println("Кристалическая руда под бафом      " + "%.0f".format(crystalMineralBuff1))
+    println("Железная руда под бафом            " + "%.0f".format(ironMineralBuff1))
+
+
+}
