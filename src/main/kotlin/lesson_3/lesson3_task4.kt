033fea05f3bb1390a44d3fa391b6838011589080
Fallien <rdp6@mail.ru>
2024-09-11 08:42:14 +0300

    KS-3-4 выполнение задания

---
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var number = "hod 1"
+    var start = "e2"
+    var finish = "e4"
+    println("$start - $finish ; $number")
+    number = "hod 2"
+    start = "d2"
+    finish = "d3"
+    println("$start - $finish ; $number")
+}
