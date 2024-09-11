ad6b6afa9c84445b2b56eab756b70fb472447c24
Fallien <rdp6@mail.ru>
2024-09-11 05:59:02 +0300

    KS-3-2 убрал пустые строки

--- src/main/kotlin/lesson3_task2.kt
+++ src/main/kotlin/lesson3_task2.kt
@@ -1,13 +1,9 @@
 fun main() {
-
     val name = "Андреева Татьяна Сергеевна"
     val year = 20
-
     val name1 = "Сидорова Татьяна Сергеевна"
     val year1 = 22
 
     println(name +"   "+ year)
     println(name1 +"   "+ year1)
-
-
 }
