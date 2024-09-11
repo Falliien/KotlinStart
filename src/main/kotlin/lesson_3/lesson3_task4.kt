9a1700aebfb74b85e1e09b9447ff54e48aeef569
Fallien <rdp6@mail.ru>
2024-09-11 22:22:19 +0300

    KS-3-4 внес измнения согласно замечаниям

--- src/main/kotlin/lesson_3/lesson3_task4.kt
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -1,10 +1,11 @@
 fun main() {
-    var number = "hod 1"
+    var number = 1
     var start = "e2"
     var finish = "e4"
-    println("$start - $finish ; $number")
-    number = "hod 2"
+    println("[$start-$finish;${number}]")
+    number ++
     start = "d2"
     finish = "d3"
-    println("$start - $finish ; $number")
+    println("[$start-$finish;${number}]")
+
 }
